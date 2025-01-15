459d75f4d73611d576236f3c42a3dd265a983503
JaceJung-dev <jacejung.dev@gmail.com>
2025-01-15 15:49:52 +0900

    core structure done

--- Assignments_3/post/urls.py
+++ Assignments_3/post/urls.py
@@ -1,4 +1,11 @@
 from django.urls import path
+from . import views
 
 app_name = "post"
-urlpatterns = []
+urlpatterns = [
+    path("", views.post_list, name="list"),
+    path("create/", views.post_create, name="create"),
+    path("<int:pk>/delete/", views.post_delete, name="delete"),
+    path("<int:pk>/", views.post_detail, name="detail"),
+    path("<int:pk>/update/", views.post_update, name="update"),
+]
--- Assignments_3/post/views.py
+++ Assignments_3/post/views.py
@@ -1,3 +1,52 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Post
+from .forms import PostForm
+from django.http import HttpResponse
 
-# Create your views here.
+
+def post_list(request):
+    posts = Post.objects.all().order_by("-id")
+    context = {"posts": posts}
+    return render(request, "post/post_list.html", context)
+
+
+def post_detail(request, pk):
+    post = Post.objects.get(pk=pk)
+    context = {"post": post}
+    return render(request, "post/post_detail.html", context)
+
+
+def post_create(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save()
+            return redirect("post:detail", post.pk)
+    else:
+        form = PostForm()
+    context = {"form": form}
+    return render(request, "post/post_create.html", context)
+
+
+def post_delete(request, pk):
+    post = Post.objects.get(pk=pk)
+    if request.method == "POST":
+        post.delete()
+        return redirect("post:list")
+    return redirect("post:detail", post.pk)
+
+
+def post_update(request, pk):
+    post = Post.objects.get(pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save()
+            return redirect("post:detail", post.pk)
+    else:
+        form = PostForm(instance=post)
+    context = {
+        "form": form,
+        "post": post,
+    }
+    return render(request, "post/post_update.html", context)
